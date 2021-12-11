29f2a46834614df234b986d01be90b28d77442c3
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-12-11 22:51:47 +0530

    added authentication slice and split each slice of store

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,4 @@
 // import { createStore } from "redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
 
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === "increment") {
@@ -43,10 +21,12 @@ const counterSlice = createSlice({
 
 // const store = createStore(counterReducer);
 
-const store = configureStore({
-  reducer: counterSlice.reducer,
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice";
+import authReducer from "./authSlice";
 
-export const counterActions = counterSlice.actions;
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
 
 export default store;
